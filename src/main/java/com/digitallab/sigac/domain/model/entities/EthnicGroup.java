0426a40e29f553ca6cca08c32435dba5faf4e1c0
cgutierrez <cgutierrez@soaint.com>
2019-04-17 18:19:45 -0500

    grupo etnico, resguardo

--- src/main/java/com/digitallab/sigac/domain/model/entities/EthnicGroup.java
+++ src/main/java/com/digitallab/sigac/domain/model/entities/EthnicGroup.java
@@ -1,4 +1,13 @@
 package com.digitallab.sigac.domain.model.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 public class EthnicGroup {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    @Column(nullable = false)
+    private String nameEthnic;
 }
