811e7e7fb82e6b514d2a96ba825494c2ac06d1a7
cgutierrez <cgutierrez@soaint.com>
2019-04-17 20:40:33 -0500

    transformer date and casefiledocument

--- src/main/java/com/digitallab/sigac/domain/model/entities/CaseFileDocument.java
+++ src/main/java/com/digitallab/sigac/domain/model/entities/CaseFileDocument.java
@@ -1,4 +1,22 @@
 package com.digitallab.sigac.domain.model.entities;
 
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
 public class CaseFileDocument {
+    @Id
+    private Long idPerson;
+    @OneToOne
+    @JoinColumn(name = "id_person")
+    @MapsId
+    private Person person;
+    @Column
+    private LocalDate caseFileDate;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    private Municipality municipality;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( referencedColumnName = "id")
+    private Department department;
 }
