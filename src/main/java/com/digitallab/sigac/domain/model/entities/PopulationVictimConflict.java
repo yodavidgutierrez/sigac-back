4f8adf9c0033ad8626f37cb2a6dd73b1323f78e6
cgutierrez <cgutierrez@soaint.com>
2019-04-17 20:10:33 -0500

    PopulationVictimConflict entity

--- src/main/java/com/digitallab/sigac/domain/model/entities/PopulationVictimConflict.java
+++ src/main/java/com/digitallab/sigac/domain/model/entities/PopulationVictimConflict.java
@@ -1,4 +1,25 @@
 package com.digitallab.sigac.domain.model.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class PopulationVictimConflict {
+
+    @Id
+    private Long idPerson;
+    @OneToOne
+    @JoinColumn(name = "id_person")
+    @MapsId
+    private Person person;
+    @Column(nullable = false)
+    private String addresPerson;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    private Municipality municipality;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( referencedColumnName = "id")
+    private Department department;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn( referencedColumnName = "id")
+    private TypeVictimConflict typeVictimConflict;
 }
