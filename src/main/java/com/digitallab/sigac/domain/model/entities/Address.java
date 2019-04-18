61e61fc1a22fe2559164392845a675f5c94f5a9b
cgutierrez <cgutierrez@soaint.com>
2019-04-17 19:39:41 -0500

    Address entity

--- src/main/java/com/digitallab/sigac/domain/model/entities/Address.java
+++ src/main/java/com/digitallab/sigac/domain/model/entities/Address.java
@@ -1,4 +1,25 @@
 package com.digitallab.sigac.domain.model.entities;
 
-public class Address {
+import javax.persistence.*;
+
+public class Address{
+
+    @Id
+    private Long idPerson;
+    @OneToOne
+    @JoinColumn(name = "id_person")
+    @MapsId
+    private Person person;
+    @Column(nullable = false)
+    private String addresPerson;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn(referencedColumnName = "id")
+    private Municipality municipality;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn( referencedColumnName = "id")
+    private Department department;
+    @OneToMany(fetch = FetchType.LAZY)
+    @JoinColumn( referencedColumnName = "id")
+    private Zone zone;
+
 }
