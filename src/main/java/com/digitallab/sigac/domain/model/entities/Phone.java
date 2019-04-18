909c00cb5cd8b40fdc1a186325ac2c575d3ad582
cgutierrez <cgutierrez@soaint.com>
2019-04-17 20:17:52 -0500

    Phone entity

--- src/main/java/com/digitallab/sigac/domain/model/entities/Phone.java
+++ src/main/java/com/digitallab/sigac/domain/model/entities/Phone.java
@@ -1,4 +1,15 @@
 package com.digitallab.sigac.domain.model.entities;
 
+import javax.persistence.*;
+
+@Entity
 public class Phone {
+    @Id
+    private Long idPerson;
+    @OneToOne
+    @JoinColumn(name = "id_person")
+    @MapsId
+    private Person person;
+    @Column(nullable = false, unique = true)
+    private String phonePerson;
 }
