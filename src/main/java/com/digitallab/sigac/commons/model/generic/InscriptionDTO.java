c5fd2c01af322a9466d8edc5db49a757af540f16
cgutierrez <cgutierrez@soaint.com>
2019-04-26 17:51:01 -0500

    fix InscriptionDTO

---
+++ src/main/java/com/digitallab/sigac/commons/model/generic/InscriptionDTO.java
@@ -0,0 +1,25 @@
+package com.digitallab.sigac.commons.model.generic;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class InscriptionDTO implements Serializable {
+    private Integer inscriptionNumber;
+    private Date inscriptionDate;
+    private Date fingeringDate;
+    private Integer year;
+    private String grade;
+    private Boolean canceled;
+    private String comments;
+    private String privateSector;
+    private String academicSituation;
+}
---
+++ src/main/java/com/digitallab/sigac/commons/model/request/InscriptionDTORequest.java
@@ -0,0 +1,16 @@
+package com.digitallab.sigac.commons.model.request;
+
+import com.digitallab.sigac.commons.model.generic.InscriptionDTO;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+@Data
+@Builder
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class InscriptionDTORequest extends InscriptionDTO implements Serializable {
+}
